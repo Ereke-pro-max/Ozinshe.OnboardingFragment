ba1244cf20ca4b6c5c3b13ec7747ab8df63d9b70
Ereke-pro-max <erzhanatslam@gmail.com>
2025-01-11 20:46:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ozinshe/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.example.ozinshe
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.ozinshe.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(3000)
+            findNavController().navigate(R.id.action_splashFragment_to_onboardingFragment)
+        }
+    }
+}
